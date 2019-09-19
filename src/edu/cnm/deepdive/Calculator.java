33bcf189ec576db056a8a6bbbaebcbd5c0a78758
Sami <samimheard@gmail.com>
2019-09-19 09:20:58 -0600

    clean up
    try with resources

--- src/edu/cnm/deepdive/Calculator.java
+++ src/edu/cnm/deepdive/Calculator.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive;
 
+import java.io.InputStream;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
@@ -7,25 +8,23 @@ import java.util.Scanner;
 
 public class Calculator {
 
-  private Scanner scanner;
-  private Deque<Double> operands;
 
   public static void main(String[] args) {
-    Calculator calc = new Calculator(new Scanner(System.in));
-    calc.process();
+    Calculator calc = new Calculator();
+    calc.process(System.in);
   }
 
-  public Calculator(Scanner scanner) {
-    this.scanner = scanner;
-    operands = new LinkedList<>();
-  }
 
-  public void process(){
-    try{
+  public void process(InputStream in) {
+    private Deque<Double> operands = new LinkedList<>();
+
+    try(Scanner scanner = new Scanner(in)){
       while (scanner.hasNextDouble()){
           operands.push(scanner.nextDouble());
         }
-      } catch (NoSuchElementException ignored){} finally {
+      } catch (NoSuchElementException ignored){
+      //end of input; complete processing
+    } finally {
       System.out.println(operands);
     }
   }
